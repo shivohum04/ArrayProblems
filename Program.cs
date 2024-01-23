b7aa2d34757e349174d53f8c61824198593a6a1c
shivohum04 <shivohum04@gmail.com>
2024-01-23 22:36:37 +0530

    [shiv]ADD:printed unique elements

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-namespace Q1 // count the number of duplicates
+namespace Q2 // Print unique elements
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            int[] numbers = new int[] { 1, 2, 3, 2, 1 };
+            int[] numbers = new int[] { 1, 2, 3, 4, 1, 5, 6 , 1,2, 3};
 
-            int count = 0;
-
-            for(int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                for(int j=i+1;j < numbers.Length; j++)
+                bool isUnique = true;
+
+                for (int j = 0; j < numbers.Length; j++)
                 {
-                    if (numbers[i] == numbers[j])
+                    if (i != j && numbers[i] == numbers[j])
                     {
-                        count++;
+                        isUnique = false;
+                        break;
                     }
                 }
+
+                if (isUnique)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
             }
-            Console.WriteLine("the number of duplicates are " + count);
         }
     }
 }
