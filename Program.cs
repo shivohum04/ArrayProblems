a35c5c230ecdd144a36c6fc320f763cb627f7b8e
shivohum04 <shivohum04@gmail.com>
2024-01-23 22:05:13 +0530

    [shiv]ADD:counting number of duplicates solved

--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+
+namespace Q1 // count the number of duplicates
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[] { 1, 2, 3, 2, 1 };
+
+            int count = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                for(int j=i+1;j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("the number of duplicates are " + count);
+        }
+    }
+}
